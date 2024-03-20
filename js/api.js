8364366319c5e40f2689efecaca157f013381489
testusuario <jonyxt1063@gmail.com>
2024-03-19 23:08:38 -0600

    tuesday-19

--- js/api.js
+++ js/api.js
@@ -16,13 +16,15 @@ async function getData(){
 
 function showHTML(data){
     const content = document.querySelector('#content');
-    let html = ''
+    let html = '';
+    let color;
     data.forEach(character => {
-        const {name, image, id} = character;
-
+        const {name, image, id, status} = character;
+        status === "Alive" ? color = "green" : status === "Dead" ? color = "red" : color = "gray";
         html += `
-            <a href="character.html?id=${id}">
-                <p class="font-bold">${name} </p>
+            <a class="bg-gray-600 p-2 rounded text-white" href="character.html?id=${id}">
+                <p class="font-bold">${name}</p>
+                <p><span class="text-${color}-600">●</span> ${status}</p>
                 <img src="${image}" alt="character">
             </a>
         `
