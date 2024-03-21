38ee2b9ea35f403fd47e7bcecb7fd52dfed74615
testusuario <jonyxt1063@gmail.com>
2024-03-21 17:36:33 -0600

    filter-working

--- js/api.js
+++ js/api.js
@@ -1,14 +1,15 @@
 const btnSearch = document.querySelector('#btnSearch');
 const btnFilter = document.querySelector('#filter');
-const selectEpisodes = document.querySelector('#episodes');
-const selectLocations = document.querySelector('#locations');
+let selectEpisodes;
+let selectLocations;
+let selectStatus;
+let selectGender;
 const btnNextPage = document.querySelector('#nextPage');
 const btnPreviousPage = document.querySelector('#previousPage');
 const divPages = document.querySelector('#pages');
+let objectCharacter;
 btnNextPage.addEventListener('click', showPage)
 btnPreviousPage.addEventListener('click', showPage)
-selectEpisodes.addEventListener('change', episodeSelected);
-selectLocations.addEventListener('change', locationSelected);
 btnFilter.addEventListener('click', filterBtn);
 btnSearch.addEventListener('click', searchCharacter);
 
@@ -21,40 +22,58 @@ async function getData(){
         const response = await fetch(`${url}/character/`);
         const result = await response.json();
         pages = result.info.pages;
+        objectCharacter = result
+        console.log(objectCharacter);
         showHTML(result.results);
+        console.log(pages);
     } catch (error){
         console.log(error);
     }
 }
 let currentPage = 1;
+
+console.log(currentPage);
 async function showPage(e){
     try{
-        if(e.target.id === 'nextPage'){
+        if(pages >= currentPage){
+            if(e.target.id === 'nextPage'){
             currentPage++;
             btnPreviousPage.classList.remove('hidden');
-        
-        }else{
-            currentPage--;
-            if(currentPage === 1){
-                btnPreviousPage.classList.add('hidden');
+            console.log(currentPage, "pagina acutal");
+            if(currentPage == pages){
+                btnNextPage.classList.add('hidden');
             }
-        }
-        console.log(currentPage);
+             
+            
+            }else{
+                if(btnNextPage.classList.contains('hidden')){
+                    btnNextPage.classList.remove('hidden')
+                }
+                currentPage--;
+                console.log(currentPage);
+                if(currentPage === 1){
+                    btnPreviousPage.classList.add('hidden');
+                }
+            }
         const response = await fetch(`${url}/character/?page=${currentPage}`);
         const result = await response.json();
         showHTML(result.results)
+        }else{
+            console.log('no more pages');
+        }
     } catch (error){
         console.log(error);
     }
 }
 
+
 function showHTML(data){
     const content = document.querySelector('#content');
     let html = '';
-    let color;
+    let color = "gray";
     data.forEach(character => {
         const {name, image, id, status} = character;
-        status === "Alive" ? color = "green" : status === "Dead" ? color = "red" : color = "gray";
+        status === "Alive" ? color = "green" : status === "Dead" ? color = "red" : null;
         html += `
             <a class="bg-gray-600 p-2 rounded text-white" href="character.html?id=${id}">
                 <p class="font-bold">${name}</p>
@@ -74,23 +93,89 @@ async function searchCharacter(e){
     try{
         const response = await fetch(`${url}/character/?name=${character}`);
         const result = await response.json();
+        console.log(result);
+        objectCharacter = result.results;
+        console.log(objectCharacter);
+        hidePagesBtn()
         showHTML(result.results);
     } catch (error){
-        console.log(error);
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "This character doesn't exist!",
+        });
     }
 }
-//side bar
-function filterBtn(){
-    const sidebar = document.querySelector('#sidebar');
-    
-    if(sidebar.classList.contains('hidden')){
-        sidebar.classList.remove('hidden');
-    }else{
-        sidebar.classList.add('hidden')
-    }
-    // sidebar.classList.toggle('-translate-x-full');
+//Filter
+async function filterBtn(){
+     Swal.fire({
+        title: "Filter by",
+        html: `
+            <form action="">
+            <div class="">
+                <label for="episodes" class="text-gray-500 font-bold">Episodes:</label>
+                <select
+                class="text-black"
+                name="episodes"
+                id="episodes"
+                form=""
+                ></select>
+            </div>
+            <p class="font-bold p-4">or</p>
+            <div class="">
+                <label class="text-gray-500 font-bold" for="locations">Locations:</label>
+                <select
+                class="text-black"
+                name="locations"
+                id="locations"
+                form=""
+                ></select>
+            </div>
+            <p class="font-bold p-4">and</p>
+            <div class="">
+                <label class="text-gray-500 font-bold" for="status">Status:</label>
+                <select
+                class="text-black"
+                name="status"
+                id="status"
+                form=""
+                >
+                <option value="Alive">Alive</option>
+                <option value="Dead">Dead</option>
+                <option value="unknown">Unknown</option>
+                </select>
+            </div>
+            <div class="">
+                <label class="text-gray-500 font-bold" for="gender">Gender:</label>
+                <select
+                class="text-black"
+                name="gender"
+                id="gender"
+                form=""
+                >
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="unknown">Unknown</option>
+                </select>
+            </div>
+            </form>
+            
+        `,
+        showConfirmButton: false
+  
+    });
+    getEpisodes();
+    getLocations();
+    selectEpisodes = document.querySelector('#episodes');
+    selectLocations = document.querySelector('#locations');
+    selectStatus = document.querySelector('#status');
+    selectGender = document.querySelector('#gender');
+    selectEpisodes.addEventListener('change', episodeSelected);
+    selectLocations.addEventListener('change', locationSelected);
+    selectStatus.addEventListener('change', statusSelected);
+    selectGender.addEventListener('change', genderSelected);
 }
-getEpisodes();
+
 async function getEpisodes(){
     let allResults = [];
     let currentPage = 1;
@@ -120,12 +205,13 @@ async function episodeSelected(){
     try {
         const response = await fetch(`${url}/episode/${Number(this.value)}`);
         const result = await response.json();
-        getCharacter(result.characters)
+        getCharacter(result.characters);
+        hidePagesBtn(true)
     } catch (error) {
         console.log(error);
     }
 }
-let object;
+
 async function getCharacter(data){
     try {
         const promises = data.map(async (character) => {
@@ -133,18 +219,15 @@ async function getCharacter(data){
             return response.json();
         });
         const characters = await Promise.all(promises);
-        // characters.forEach(character => {
-        //     character.location.name.includes('Earth') ? console.log(character.name) : console.log();
-        // })
-        object = characters;
-        console.log(object);
+        objectCharacter = characters;
+        console.log(objectCharacter);
         showHTML(characters);
     } catch (error) {
         console.log(error);
     }
 }
 //locations
-getLocations();
+
 async function getLocations(){
     let allResults = [];
     let currentPage = 1;
@@ -164,6 +247,7 @@ async function locationSelected(){
         const response = await fetch(`${url}/location/${Number(this.value)}`);
         const result = await response.json();
         getCharacter(result.residents);
+        hidePagesBtn(true)
         console.log(result);
     } catch (error) {
         console.log(error);
@@ -178,3 +262,37 @@ function showLocatons(data){
     });
     selectLocations.innerHTML = html
 };
+
+async function statusSelected(){
+    if(objectCharacter.info){
+        const response = await fetch(`${url}/character/?status=${this.value}`);
+        const result = await response.json();
+        console.log(result);
+        showHTML(result.results);
+    }else{
+        let arrayCharacters = objectCharacter.filter(character => character.status === this.value );
+        showHTML(arrayCharacters);
+    }
+}
+
+async function genderSelected(){
+    if(objectCharacter.info){
+        const response = await fetch(`${url}/character/?gender=${this.value}`);
+        const result = await response.json();
+        console.log(result);
+        showHTML(result.results);
+    }else{
+        let arrayCharacters = objectCharacter.filter(character => character.gender === this.value );
+        showHTML(arrayCharacters);
+    }
+    
+}
+
+function hidePagesBtn(hide){
+    divPages.classList.add('hidden')
+    if(!hide){
+        console.log('si');
+    }else{
+        console.log('no');
+    }
+}
